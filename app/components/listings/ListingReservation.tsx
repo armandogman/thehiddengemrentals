b41aa2a71e98878ad0380116f492883b18be7535
armandogman <armandogman880@gmail.com>
2023-08-05 11:49:41 -0500

    price changes commit

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -76,7 +76,7 @@ const ListingReservation: React.FC<
           Total
         </div>
         <div>
-          $ {totalPrice}
+          $ {totalPrice +150}
         </div>
       </div>
     </div>
